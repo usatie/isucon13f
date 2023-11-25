4c749eed1420e369bb7281911b407ab8ab65c124
isucon <isucon@example.com>
2023-11-25 03:02:42 +0000

    [名前解決成功数 5469 売上: 9725] #5 Add index

--- webapp/sql/init.sql
+++ webapp/sql/init.sql
@@ -23,3 +23,24 @@ ALTER TABLE `tags` auto_increment = 1;
 ALTER TABLE `livecomments` auto_increment = 1;
 ALTER TABLE `livestreams` auto_increment = 1;
 ALTER TABLE `users` auto_increment = 1;
+
+DROP INDEX `livestream_id_idx` ON `livestream_tags`;
+ALTER TABLE livestream_tags ADD INDEX livestream_id_idx(livestream_id);
+
+DROP INDEX `user_id_idx` ON `icons`;
+ALTER TABLE icons ADD INDEX user_id_idx(user_id);
+
+DROP INDEX `name_idx` ON `isudns`.records;
+ALTER TABLE `isudns`.records ADD INDEX name_idx(name);
+
+DROP INDEX `livestream_id_idx` ON reactions;
+ALTER TABLE reactions ADD INDEX livestream_id_idx(livestream_id);
+
+DROP INDEX `livestream_id_idx` ON livecomments;
+ALTER TABLE livecomments ADD INDEX livestream_id_idx(livestream_id);
+
+DROP INDEX `user_id_idx` ON themes;
+ALTER TABLE themes ADD INDEX user_id_idx(user_id);
+
+DROP INDEX `start_end_idx` ON reservation_slots;
+ALTER TABLE reservation_slots ADD INDEX start_end_idx(start_at, end_at);
